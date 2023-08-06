6a58eccace0027b6a118914600d27e83b37c05e4
Ivo Fehn <ivo.fehn@gmail.com>
2023-08-06 14:52:29 +0200

    Minor typ fix

--- src/utils/imageByIndex.ts
+++ src/utils/imageByIndex.ts
@@ -1,9 +1,11 @@
+import { StaticImageData } from "next/image";
 import image1 from "../../public/slide-1.webp";
 import image2 from "../../public/slide-2.webp";
 import image3 from "../../public/slide-3.webp";
 
-export const images: string[] = [image1, image2, image3];
+export const images: StaticImageData[] = [image1, image2, image3];
 
-const imageByIndex = (index: number): string => images[index % images.length];
+const imageByIndex = (index: number): StaticImageData =>
+  images[index % images.toString().length];
 
 export default imageByIndex;
