a3bddda7134db5f0aea3195c4a0fc4b6a78da730
Ivo Fehn <ivo.fehn@gmail.com>
2023-08-23 19:51:39 +0200

    Styling fixes in testimonial

--- src/components/TestimonialsComponent.tsx
+++ src/components/TestimonialsComponent.tsx
@@ -27,20 +27,17 @@ const testimonials: Testimonial[] = [
 
 const TestimonialsComponent = (props: Props) => {
   return (
-    <section className="4k:px-[20vw] xl:px-0 mx-auto shadow-sm border-colorBorderLightGrey border-solid border-2 rounded-3xl flex mt-3 lg:flex-col lg:items-center lg:justify-center lg:px-4 lg:border-none lg:shadow-none">
+    <section className="4k:px-[20vw] xl:px-0 shadow-sm border-colorBorderLightGrey border-solid border-2 rounded-3xl flex mt-3 lg:flex-col lg:items-center lg:justify-center lg:px-4 lg:border-none lg:shadow-none">
       <div className="p-3">
         <Image
           src={"/service-picture.jpg"}
           width={400}
           height={400}
           alt="Testimonial Picture"
-          style={{
-            borderTopLeftRadius: "1rem",
-            borderBottomLeftRadius: "1rem",
-          }}
+          className="testimonial-image"
         ></Image>
       </div>
-      <div className="text-colorPrimary flex justify-center items-center flex-col w-3/4 lg:pt-24">
+      <div className="text-colorPrimary flex justify-center items-center flex-col w-3/4 lg:pt-24 lg:text-center">
         <h4 className="text-xl font-semibold">Out Test</h4>
         <h2 className="text-6xl font-semibold mb-20">Our Client feedback</h2>
         <TestimonialSlider testimonials={testimonials} />
